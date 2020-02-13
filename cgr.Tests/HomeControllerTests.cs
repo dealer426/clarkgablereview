9fbc829a7aad0fc80aed731259f7834737f6f889
dealer426 <burnssamuel01@gmail.com>
2020-02-12 19:42:11 -0500

    changed startup file

---
+++ cgr.Tests/HomeControllerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace cgr.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_Returns_A_View()
+        {
+            var underTest = new HomeController();
+
+            var result = underTest.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+
+    }
+}
